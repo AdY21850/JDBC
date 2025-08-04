3a6844a14c00143e2e652e11e918c41ee5701bec
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-08-05 02:23:29 +0530

    created Users class 😁😉

--- Banking management System/src/BankingManagementSystem/Users.java
+++ Banking management System/src/BankingManagementSystem/Users.java
@@ -1,4 +1,24 @@
 package BankingManagementSystem;
-
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 public class Users {
+    private Connection con;
+    private Scanner sc;
+    public Users(Connection con,Scanner sc){
+    this.con=con;
+    this.sc=sc;
+    }
+    public void register(){
+        sc.nextLine();
+        System.out.println("Full Name: ");
+        String full_name=sc.nextLine();
+        System.out.println
+                ("Enter the email : ");
+        String email=sc.nextLine();
+        System.out .println("Enter password: ");
+    }
 }
