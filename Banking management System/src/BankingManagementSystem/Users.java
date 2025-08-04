df0e4aa4d43f0fca9f73f887d4166e57596fd0d8
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-08-05 02:40:15 +0530

    creates Users class 😉😁

--- Banking management System/src/BankingManagementSystem/Users.java
+++ Banking management System/src/BankingManagementSystem/Users.java
@@ -20,5 +20,65 @@ public class Users {
                 ("Enter the email : ");
         String email=sc.nextLine();
         System.out .println("Enter password: ");
+        String password = sc.nextLine();
+        if (user_exist(email)){
+            System.out.println("User already exists for this user");
+            return;
+        }
+        String register_query="INSERT INTO User(full_name,email,password) VALUES(?,?,?)";
+        try{
+            PreparedStatement ps=con.prepareStatement(register_query);
+            ps.setString(1,full_name);
+            ps.setString(2,email);
+            ps.setString(3,password);
+            int rowsaffected=ps.executeUpdate();
+            if(rowsaffected>0){
+                System.out.println("Registration Succesfull!!");
+            }
+            else{
+                System.out.println("Registration Failed !!!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public String login(){
+        sc.nextLine();
+        System.out.println("Email: ");
+        String email=sc.nextLine();
+        System.out.println("Password: ");
+        String password=sc.nextLine();
+        String login_query="SELECT*FROM User WHERE email =? AND password=?";
+        try{
+            PreparedStatement ps=con.prepareStatement(login_query);
+            ps.setString(1,email);
+            ps.setString(2,password);
+            ResultSet rs=ps.executeQuery();
+            if (rs.next()){
+                return email;
+            }
+            else{
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public boolean user_exist(String email){
+        String query="SELECT* FROM user WHERE email=?";
+        try{
+            PreparedStatement ps=con.prepareStatement(query);
+            ps.setString(1,email);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
