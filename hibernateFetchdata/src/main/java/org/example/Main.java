3c3bed4c1b45221d1b046cbbb0be71b144704a62
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-08-16 19:27:47 +0530

    fetch data from mysql db using hibernate

---
+++ hibernateFetchdata/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+class Main{
+    public static void main(String args[]){
+        employee emp=new employee();
+        emp.setId(12);
+        emp.setName("krishna");
+        emp.setJob("java full Stack");
+        emp.setSalary(15000);
+
+        Configuration config=new Configuration();
+        config.addAnnotatedClass(org.example.employee.class);
+        config.configure();
+
+
+        SessionFactory factory= config.buildSessionFactory();
+        Session session= factory.openSession();
+        Transaction tx= session.beginTransaction();
+        session.persist(emp);
+
+        tx.commit();
+        session.close();
+        factory.close();
+    }
+}
